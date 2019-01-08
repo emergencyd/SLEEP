08c78897bf1f4ae36212e266c885cbb430195775
emergencyd <cxwldsj@gmail.com>
2019-01-08 11:34:39 +0800

    Update loadedf.m

--- loadedf.m
+++ loadedf.m
@@ -2,7 +2,7 @@ clear
 path = cd;
 path_data = [path, '\EDF_FILES'];
 dir_data = dir(fullfile(path_data,'*.edf'));
-data_Names = {dir_data.name}';
+data_Names = {dir_data.name};
 for i = 1: length(data_Names)
     file = [path_data, '\', data_Names{i}];
     [hdr{i},record] = edfread(file);
