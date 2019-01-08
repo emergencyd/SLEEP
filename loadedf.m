ee2c45f5b34ca9ea0987833491b9888b20000e51
emergencyd <cxwldsj@gmail.com>
2019-01-08 11:32:28 +0800

    Update loadedf.m

--- loadedf.m
+++ loadedf.m
@@ -1,11 +1,11 @@
 clear
-path=cd;
-path_data=[path,'\EDF_FILES'];
-dir_data=dir(fullfile(path_data,'*.edf'));
-data_Names={dir_data.name}';
-for i=1:length(data_Names)
-    file=[path_data,'\',data_Names{i}];
-    [hdr{i},record]=edfread(file);
-    name_data=['record',num2str(i)];
-    save(name_data,'record')
+path = cd;
+path_data = [path, '\EDF_FILES'];
+dir_data = dir(fullfile(path_data,'*.edf'));
+data_Names = {dir_data.name}';
+for i = 1: length(data_Names)
+    file = [path_data, '\', data_Names{i}];
+    [hdr{i},record] = edfread(file);
+    name_data = ['record', num2str(i)];
+    save(name_data, 'record')
 end
